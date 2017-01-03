beda8f398da69a98bd9dedb289a86797ce07d77d
c1rabbit <mail@calvinthanh.com>
2017-01-02 20:43:02 -0800

    session store configured with mongodb

--- config/session.js
+++ config/session.js
@@ -75,9 +75,9 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-  // adapter: 'mongo',
-  // url: 'mongodb://user:password@localhost:27017/dbname', // user, password and port optional
-
+  adapter: 'mongo',
+  //url: 'mongodb://user:password@localhost:27017/dbname', // user, password and port optional
+  url: 'mongodb://localhost:27017/sessions',
   /***************************************************************************
   *                                                                          *
   * Optional Values:                                                         *
